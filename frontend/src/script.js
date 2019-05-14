aa67ce08125db9da2feae211df2e60e003bc1dba
Thomas Jaggi <thomas@responsive.ch>
2019-05-14 07:49:24 +0200

    Rewrite in ES5

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -1,32 +1,40 @@
 const API = "http://localhost:8080/wp-json/wp/v2/posts";
 
-async function getPosts() {
-  const response = await fetch(API);
-  const posts = await response.json();
-
-  return posts;
+function getPosts() {
+  return fetch(API).then(function(response) {
+    return response.json();
+  });
 }
 
 getPosts()
-  .then(posts => {
-    posts.forEach(post => {
+  .then(function(posts) {
+    posts.forEach(function(post) {
       console.log(post);
 
       const element = document.createElement("div");
       const target = document.querySelector("[data-target]");
 
-      element.innerHTML = `
-<h2>${post.title.rendered}</h2>
-${post.content.rendered}
-<ul>
-${Object.entries(post.custom)
-  .filter(([name]) => !name.startsWith("_"))
-  .map(([name, value]) => `<li><strong>${name}</strong>: ${value}</li>`)
-  .join("")}
-</ul>
-`;
+      element.innerHTML =
+        "<h2>" +
+        post.title.rendered +
+        "</h2>" +
+        post.content.rendered +
+        "<ul>" +
+        Object.keys(post.custom)
+          .filter(function(name) {
+            return !name.match(/^_/);
+          })
+          .map(function(name) {
+            return (
+              "<li><strong>" + name + "</strong>:" + post.custom[name] + "</li>"
+            );
+          })
+          .join("") +
+        "</ul>";
 
       target.appendChild(element);
     });
   })
-  .catch(err => console.error(err));
+  .catch(function(err) {
+    console.error(err);
+  });
